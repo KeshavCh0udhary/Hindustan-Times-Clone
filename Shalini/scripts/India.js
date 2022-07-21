35c6f5c153c8cc98c1afd19bb157bab2478b9c44
KeshavCh0udhary <Krishna2000chy@gmail.com>
2022-07-21 22:20:23 +0530

    kk

--- Shalini/scripts/India.js
+++ Shalini/scripts/India.js
@@ -11,60 +11,18 @@ let getData = async() => {
 
 getData()
 
-// -------------------------------------------Top Front News------------------------
-
-
-// let big_div = document.createElement("div");
-// big_div.setAttribute("id", "big_div");
-
-//     let head =  document.createElement("h3");
-//     head.innerText = "INDIA NEWS";
-
-//     let title =  document.createElement("h3");
-//     title.innerText = "IPL Team Owners Snap Up All 6 Franchises In New South Africa T20 League - NDTV Sports";
-
-//     let image =  document.createElement("img");
-//     image.src = "https://c.ndtvimg.com/2022-02/8dvukpg_ipl-auction-2022-flags_625x300_12_February_22.jpg?im=FeatureCrop,algorithm=dnn,width=1200,height=675";
-
-//     let dateAndtime  =  document.createElement("h4");
-//     dateAndtime.innerText = "2022-07-20T07:43:32Z" ;
-
-//     let icon =  document.createElement("div");
-
-//        let sharei =  document.createElement("i");
-//        sharei.setAttribute("class","fa fa-share")
-
-//        let bokmark =  document.createElement("i");
-//        bokmark.setAttribute("class","fa fa-bookmark")
-
-//     icon.append(sharei, bokmark)
-
-// big_div.append(head, title, image, dateAndtime, icon)
-
-
-
-
-
-
-
-
-
 
 let appendData = (data) => {
 
  let container = document.getElementById("middle-section");
 
  data.forEach(function(ele){
-    //  console.log(ele)
-// -------------------------------------------Bottom small Div News---------------------------------------------
-
-    let div = document.createElement("div");
-    div.setAttribute("class","news_div");
-    // div.addEventListener("click", () =>{
-    //     details(ele)
-    // })
+let div = document.createElement("div");
+div.setAttribute("class","news_div");
+div.addEventListener("click", () =>{
+   details(ele)
+})
     
-  
       let div1 = document.createElement("div");
       div1.setAttribute("class","news_div1");
   
@@ -115,68 +73,52 @@ let appendData = (data) => {
 
 }
 
-// function details(ele) {
-//   console.log(ele)
-//  let container = document.getElementById("middle-section");
-//   let div = document.createElement("div")
-
-//     let titlee = document.createElement("h3")
-//     titlee.innerText = ele.title;
-
-//     let imgz = document.createElement("img")
-//     imgz.src = ele.urlToImage;
-
-//     let desc = document.createElement("p")
-//     desc.innerText = ele.description;
-
-//   div.append(titlee, imgz, desc)
-//   container.append(div)
-
-
-
-// }
-
-// // ===================================== Left-Section News ====================================================
-
-// let url = ``
-
-// let getData = async() => {
-//     let res = await fetch (url)
-//     let data = await res.json()
-
-//     console.log(data)
-//     appendData(data)
-// }
-
-
-// let appendData = () => {
-
-//     let container = document.getElementById("left-section");
+function details(ele) {
+    console.log(ele)
+   let container = document.getElementById("middle-section");
+   container.innerHTML = null;
+    let div = document.createElement("div")
+    div.setAttribute("class","detailzz_div");
+  
+      let titlee = document.createElement("h3")
+      titlee.innerText = ele.title;
+  
+      let imgz = document.createElement("img")
+      imgz.src = ele.urlToImage;
+  
+      let desc = document.createElement("p")
+      desc.innerText = ele.description;
+  
+      let mix_div = document.createElement("div");
+            mix_div.setAttribute("id","icons_div")
+  
+                  let date =  document.createElement("p");
+                  date.innerText = ele.publishedAt;
+    
+                  let icons =  document.createElement("div");
+                  icons.setAttribute("id","icons")
+                  
+                      let share =  document.createElement("i");
+                      share.setAttribute("class","fa fa-share-alt")
+          
+                      let bookmark =  document.createElement("i");
+                      bookmark.setAttribute("class","fa fa-bookmark-o")
+          
+                  icons.append(share, bookmark);
+  
+              mix_div.append(date, icons)
+  
+      let link = document.createElement("iframe")
+      link.src = ele.url;
+  
+      let content = document.createElement("p")
+      content = ele.content;
+  
+      let advertisements = document.createElement("div");
+      advertisements.setAttribute("id", "new_data")
+      advertisements.addEventListener("click",getData())
    
-//     data.forEach(function(ele){
-
-
-//         let div = document.createElement("div");
-//         div.setAttribute("id", "left_topnews_div");
-    
-//             let heading =  document.createElement("h2");
-//             heading.innerText = ;
-    
-//             let title =  document.createElement("h3");
-//             title.innerText = ;
-    
-//             let dateAndtime  =  document.createElement("h3");
-//             dateAndtime.innerText = ;
-    
-//         div.append(heading, title, dateAndtime)
-
-
-//     });
-
-// };
-
-
-
-
-
-// // ============================================ Right-Section News ===================================================
+    div.append(titlee, imgz, desc, mix_div, link, content, advertisements)
+    container.append(div)
+  
+  }
