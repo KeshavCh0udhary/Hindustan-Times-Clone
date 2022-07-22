8cb76ab1660315e9af3d436b80bc5327f773878f
Rahamat725 <101379670+Rahamat725@users.noreply.github.com>
2022-07-22 21:18:41 +0530

    Add files via upload

--- Rahamat/script/world.js
+++ Rahamat/script/world.js
@@ -7,7 +7,7 @@ async function getData() {
   let data= await res.json();
 //   console.log(data.articles);
   append(data.articles);
-  headLine(data.articles);
+  // headLine(data.articles);
 
 }
 
@@ -16,11 +16,14 @@ getData();
 // --------------------For Middle Content---------------------------------------------
 
 function append(data) {
-console.log(data);
+// console.log(data);
 let container=document.getElementById("content_middle");
 
 data.forEach((el)=>{
-let heading=document.createElement("h3");
+  let top=document.createElement("h3")
+  top.innerText="WORLD NEWS";
+
+let heading=document.createElement("h2");
 heading.innerText=el.title;
 
 let des=document.createElement("p");
@@ -37,27 +40,73 @@ let pub=document.createElement("p");
 pub.innerText="Published on "+el.publishedAt;
 
 let div=document.createElement('div');
-div.append(heading,des,image,content,pub);
+div.append(top,heading,des,image,content,pub);
 container.append(div);
 })
 }
 
 // --------------------------for Left content----------------------------------------
 
+async function leftSection() {
+  const url=`https://newsapi.org/v2/top-headlines?country=IN&apiKey=6f85ccbc79a94217a85abd703fd4e188`;
+let res=await fetch(url);
+let data= await res.json();
+//   console.log(data.articles);
+
+headLine(data.articles);
+
+}
+leftSection();
+
 function headLine(data) {
 let content= document.getElementById("content_left");
 data.forEach((el)=>{
-  let h3=document.createElement("h3");
+  let top=document.createElement("h3")
+  top.innerText="INDIA NEWS";
+  let h3=document.createElement("h4");
   h3.innerText=el.title;
   let pub=document.createElement("p");
   pub.innerText="Published on "+el.publishedAt;
 
   let div=document.createElement('div');
   
-  div.append(h3,pub);
+  div.append(top,h3,pub);
   content.append(div);
   
 })
 }
 
+// -----------RIGHT CONTENT------------------
 
+// https://newsapi.org/v2/everything?q=Apple&from=2022-07-22&sortBy=popularity&apiKey=API_KEY
+
+async function rightSection() {
+  const url=`https://newsapi.org/v2/everything?q=Bollywood&from=2022-07-22&sortBy=popularity&apiKey=6f85ccbc79a94217a85abd703fd4e188`;
+let res=await fetch(url);
+let data= await res.json();
+  console.log(data.articles);
+
+topHeadLine(data.articles);
+
+}
+rightSection();
+
+function topHeadLine(data) {
+let content= document.getElementById("content_right");
+data.forEach((el)=>{
+  let top=document.createElement("img")
+  top.src=el.urlToImage;
+  top.setAttribute("class","sizeImg");
+
+  let h3=document.createElement("h4");
+  h3.innerText=el.title;
+  let pub=document.createElement("p");
+  pub.innerText="Published on "+el.publishedAt;
+
+  let div=document.createElement('div');
+  
+  div.append(h3,top,pub);
+  content.append(div);
+  
+})
+}
