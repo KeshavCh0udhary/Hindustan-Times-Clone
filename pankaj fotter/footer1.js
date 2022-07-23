51b42fd735cacf2092d3d6731aea70cf8394ed8c
Pankaj Rai <pankajrai3295@gmail.com>
2022-07-23 15:54:40 +0530

    fotter pankaj

---
+++ pankaj fotter/footer1.js
@@ -0,0 +1,76 @@
+let fotter = ()=> {
+    return `<div id="bg">
+    <div id="container" >
+        <div id="top-top">
+            <img src="https://www.hindustantimes.com/res/images/ht-logo.svg" alt="">
+            <img src="https://images.hindustantimes.com/images/app-images/2022/2/mweb_button_svg.svg">
+        </div>
+        <h2>
+            Subscribe to our best newsletters
+        </h2>
+        <div id="top-mid" >
+            <ul>
+                <li>
+                    <div id="first" > 
+                        <ul>
+                            <li>
+                                <select name="HT Daily Capsule" id="sel">
+                                    <option value="">HT Daily Capsule</option>
+                                    <option value="">@HT_Ed Calling</option><br>
+                                    <option value="">HT Wknd</option><br>
+                                    <option value="">HT Week Ahead</option><br>
+                                    <option value="">HT Capital Letters</option><br>
+                                    <option value="">Wired Wisdom</option><br>
+                                    <option value="">HT City</option><br>
+                                    <option value="">HT Mind the Gap</option><br>
+                                    <option value="">HT Kick Off</option><br>
+                                </select>
+                            </li>
+                            <li>
+                                <input type="text" placeholder="Enter your E-mail">                  
+                            </li>
+                            <li>
+                                <button id="btn">SUBSCRIBE</button>
+                            </li>
+                            
+                        </ul>              
+                        
+                    </div>
+                </li>
+                <li>
+                    <div id="social">
+                        <ul>
+                            <li><img src="./pankaj fotter/image/fb-logo.svg" alt=""></li>
+                            <li><img src="./pankaj fotter/image/in-logo.svg" alt=""></li>
+                            <li><img id="insta" src="./pankaj fotter/image/insta.png" alt=""></li>
+                            
+                            <li><img src="./pankaj fotter/image/tw-logo.svg" alt=""></li>
+                            <!-- <li><img src="https://images.hindustantimes.com/images/app-images/2022/1/footer-icons/ht-bangla.svg" alt=""></li> -->
+                        </ul>               
+                    </div>
+                </li>
+                
+            </ul>
+                        
+        </div><br>
+        <hr>
+        <br>
+        <div id="top-bottom">
+        </div>
+    </div>
+</div>
+    
+<div id="bottom">
+    <div id="container1">
+        <div id="bottom-top"></div>
+        <hr>
+        <br>
+        <div id="bottom-bot" ></div>
+        <hr>
+        <br>
+        <p>© 2022 HindustanTimes</p>
+    </div>        
+</div>`  
+}
+
+export default fotter
